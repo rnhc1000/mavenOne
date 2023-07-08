ee8d56685ba4d1c1d9b7da19243291b7b63b6f9b
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-07-08 20:17:33 -0300

    Adjusting Java SDK version according to Jenkins requirements

--- src/main/java/br/dev/ferreiras/App.java
+++ src/main/java/br/dev/ferreiras/App.java
@@ -1,8 +1,10 @@
 package br.dev.ferreiras;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -13,9 +15,11 @@ public class App
     public static void main( String[] args )
     {
         List<Integer> lists = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+        List<Integer> pairsOnly = new ArrayList<>();
 
         lists.stream().forEach((e) -> {System.out.println(e); });
         lists.forEach(list -> System.out.print(list.toString()+ " "));
+        lists.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
 
         for(Integer a: lists){
             System.out.print(a + " ");
