f9f27aa0dfd0c94e43eaaa285bca1d63fab01071
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-07-08 08:50:01 -0300

    Jenkins second round

--- src/main/java/br/dev/ferreiras/App.java
+++ src/main/java/br/dev/ferreiras/App.java
@@ -1,5 +1,8 @@
 package br.dev.ferreiras;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        for(Integer a: list){
+            System.out.print(a + " ");
+        }
         String s = "Hello World";
         System.out.println(s );
     }
